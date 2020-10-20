fa9446dbaf84e9246a1f2195bd6e65582d38ae8b
yupan <yupan@yijiupi.cn>
2020-10-20 11:39:58 +0800

    基数排序

--- src/main/java/com/example/arithmetic/sort/RadixSort.java
+++ src/main/java/com/example/arithmetic/sort/RadixSort.java
@@ -2,11 +2,12 @@ package com.example.arithmetic.sort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 《基数排序》
  *      基数排序 (Radix Sort) 是一种非比较型整数排序算法，其原理是将整数按位数切割成不同的数字，然后按每个位数分别比较。
+ *   基数排序可以理解成是建立在“计数排序”的基础之上的一种排序算法。
  *   基数排序的发明可以追溯到 1887 年赫尔曼·何乐礼在打孔卡片制表机 (Tabulation Machine)上的贡献。
  *   基数排序法会使用到桶 (Bucket)，顾名思义，通过将要比较的位（个位、十位、百位…），将要排序的元素分配至 0~9 个桶中，
  *   借以达到排序的作用，在某些时候，基数排序法的效率高于其它的比较性排序法。
@@ -22,7 +23,7 @@ import java.util.LinkedList;
  *      2、从最低位开始，依次进行一次排序
  *      3、从最低位排序一直到最高位排序完成以后, 数列就变成一个有序序列
  */
-public class RadioSort {
+public class RadixSort {
 
     public static void sort(int[] arr) {
         if (arr == null || arr.length < 2) {
@@ -46,23 +47,23 @@ public class RadioSort {
         }
 
         // 创建10个桶
-        ArrayList<LinkedList<Integer>> bucketList = new ArrayList<>(10);
+        List<List<Integer>> bucketList = new ArrayList<>(10);
         //初始化桶
         for (int i = 0; i < 10; i++) {
-            bucketList.add(new LinkedList<Integer>());
+            bucketList.add(new ArrayList<>());
         }
 
         // 进行每一趟的排序，从个位数开始排
-        for (int i = 1; i <= num; i++) {
+        for (int i = 0; i < num; i++) {
             for (int j = 0; j < n; j++) {
                 // 获取每个数对应位数的数字
-                int radio = (arr[j] / (int) Math.pow(10, i - 1)) % 10;
+                int radio = (arr[j] / (int) Math.pow(10, i)) % 10;
                 //放进对应的桶里
                 bucketList.get(radio).add(arr[j]);
             }
             //合并放回原数组
             int k = 0;
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < bucketList.size(); j++) {
                 for (Integer t : bucketList.get(j)) {
                     arr[k++] = t;
                 }
@@ -73,9 +74,9 @@ public class RadioSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,4,9,6,7,5,3,8,2};
+        int[] arr = SortUtil.getArr(8);
+        System.out.println("基数排序前：" + Arrays.toString(arr));
         sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("基数排序后：" + Arrays.toString(arr));
     }
-
 }
